7c183026594f4aaf71a042431a562580ea47a86b
yubin1010 <46951048+yubin1010@users.noreply.github.com>
2022-04-21 20:01:09 +0900

    Update jedis.java

--- jedis-test/src/main/java/com/example/jedis.java
+++ jedis-test/src/main/java/com/example/jedis.java
@@ -5,7 +5,7 @@ import redis.clients.jedis.Jedis;
 public class jedis {
     public static void main(String[] args) {
 
-		Jedis jedis = new Jedis("192.168.179.12",6379);
+		Jedis jedis = new Jedis("[ip]",6379);
 		jedis.set("jedis", "jedis redis java client!");
 		String value = jedis.get("jedis");
 		
